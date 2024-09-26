defbc73e619512992c28e73ce13b6395e8f14374
Ariel Guareschi <arielguareschi@gmail.com>
2024-09-25 22:24:26 -0300

    add api NoBank

---
+++ Nobank/src/test/java/br/edu/famper/nobank/NobankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.nobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
